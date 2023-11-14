3684ab17ac6667c70d9d7ff04823e4786ff69999
Julian Frimmel <julian@fri-me.de>
2023-11-14 11:24:26 +0100

    Use a lazily-initialized buffer type
    This commit abstracts away the lazy-initialization into a new (private)
    type `LazyInitializedBuffer`. This makes sure, that the initialization
    is transparent, and, in contrast to the earlier approach, cannot be for-
    gotten.
    
    Currently, there are no specific tests, as the buffer is entirely trans-
    parent.

--- src/raw_allocator/buffer.rs
+++ src/raw_allocator/buffer.rs
@@ -18,15 +18,15 @@ pub struct ValidatedOffset(usize);
 
 /// The buffer memory backing the heap.
 #[repr(align(4))]
-pub struct Buffer<const N: usize>([MaybeUninit<u8>; N]);
+pub struct Buffer<const N: usize>(LazyInitializedBuffer<N>);
 impl<const N: usize> Buffer<N> {
     /// Create a new buffer.
     ///
-    /// This buffer will be uninitialized except for the first few bytes: the
-    /// firs `HEADER_SIZE` bytes are initialized to zero. The caller must
-    /// subsequently call [`Buffer::ensure_initialization()`] to initialize the
-    /// first bytes as the header. This header is a free [`Entry`] with the size
-    /// of the remaining buffer.
+    /// This buffer will be initially uninitialized and will be initialized on
+    /// the first use. This will write the first header, which is a free
+    /// [`Entry`] with the size of the remaining buffer. This
+    /// lazy-initialization is necessary to ensure, that the buffer does not end
+    /// up in the `.data`-segment.
     ///
     /// # Panics
     /// This function panics if the buffer is less than 4 bytes in size, i.e. if
@@ -35,38 +35,7 @@ impl<const N: usize> Buffer<N> {
         assert!(N >= HEADER_SIZE, "buffer too small, use N >= 4");
         assert!(N % HEADER_SIZE == 0, "memory size has to be divisible by 4");
 
-        // initialize the header bytes to zero to make sure, that we can check
-        // against zero later without undefined behavior.
-        let mut buffer = [MaybeUninit::uninit(); N];
-        buffer[0] = MaybeUninit::new(0x00);
-        buffer[1] = MaybeUninit::new(0x00);
-        buffer[2] = MaybeUninit::new(0x00);
-        buffer[3] = MaybeUninit::new(0x00);
-        Self(buffer)
-    }
-
-    /// Ensure, that the buffer is initialized.
-    ///
-    /// This separate step is necessary to make sure, that the buffer is not
-    /// initialized with non-zero data in the binary (i.e when calling `new()`).
-    pub fn ensure_initialization(&mut self) {
-        let buffer = &mut self.0;
-        let not_yet_initialized = buffer
-            .iter_mut()
-            .take(HEADER_SIZE)
-            // SAFETY: the bytes of the first entry are initialized in `new()`
-            .map(|byte| unsafe { byte.assume_init() })
-            .all(|byte| byte == 0x00);
-
-        if not_yet_initialized {
-            let remaining_size = N - HEADER_SIZE;
-            let initial_entry = Entry::free(remaining_size).as_raw();
-
-            buffer[0] = MaybeUninit::new(initial_entry[0]);
-            buffer[1] = MaybeUninit::new(initial_entry[1]);
-            buffer[2] = MaybeUninit::new(initial_entry[2]);
-            buffer[3] = MaybeUninit::new(initial_entry[3]);
-        }
+        Self(LazyInitializedBuffer::new())
     }
 
     /// Obtain a reference to an [`Entry`] inside of the buffer.
@@ -265,6 +234,68 @@ impl<'buffer, const N: usize> Iterator for EntryIter<'buffer, N> {
     }
 }
 
+/// A `[MaybeUninit<u8>; N]`, which gets initialized and thus is free heap on
+/// the first use.
+///
+/// This buffer is written in such a way, that it is not initialized with
+/// non-zero values. This makes sure, that it does not end up in the `.data`
+/// section of the final binary.
+#[repr(C, align(4))]
+struct LazyInitializedBuffer<const N: usize> {
+    /// The inner buffer. This is fully uninitialized when created with
+    /// [`LazyInitializedBuffer::new()`].
+    buffer: core::cell::UnsafeCell<[MaybeUninit<u8>; N]>,
+    /// Flag, whether or not this buffer was already initialized.
+    is_initialized: core::cell::Cell<bool>,
+}
+impl<const N: usize> LazyInitializedBuffer<N> {
+    /// Create a new ` LazyInitializedBuffer`.
+    // TODO: this should really take a callback on how to do the initialization,
+    // so that the `LazyInitializedBuffer` does not need to know, what an entry
+    // is and how it should be stored in the buffer. This is currently not
+    // possible, as this would require mutable references to the buffer in a
+    // `const` context.
+    pub const fn new() -> Self {
+        Self {
+            buffer: core::cell::UnsafeCell::new([MaybeUninit::uninit(); N]),
+            is_initialized: core::cell::Cell::new(false),
+        }
+    }
+}
+impl<const N: usize> core::ops::Deref for LazyInitializedBuffer<N> {
+    type Target = [MaybeUninit<u8>; N];
+
+    /// Index into the (lazily-initialized) buffer.
+    ///
+    /// If necessary, this will initialize the buffer, if it is not yet
+    /// initialized.
+    fn deref(&self) -> &Self::Target {
+        if !self.is_initialized.get() {
+            // write the first entry to the buffer
+            let remaining_size = N - HEADER_SIZE;
+            let initial_entry = Entry::free(remaining_size).as_raw();
+            let buffer = unsafe { self.buffer.get().as_mut().unwrap() };
+            buffer[0] = MaybeUninit::new(initial_entry[0]);
+            buffer[1] = MaybeUninit::new(initial_entry[1]);
+            buffer[2] = MaybeUninit::new(initial_entry[2]);
+            buffer[3] = MaybeUninit::new(initial_entry[3]);
+
+            self.is_initialized.set(true);
+        }
+        unsafe { self.buffer.get().as_ref().unwrap() }
+    }
+}
+impl<const N: usize> core::ops::DerefMut for LazyInitializedBuffer<N> {
+    /// Index into the already initialized buffer.
+    ///
+    /// This must not be called before a non-mutable index operation, as only
+    /// this operation initializes the buffer.
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        debug_assert!(self.is_initialized.get());
+        self.buffer.get_mut()
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::{Buffer, Entry, ValidatedOffset, HEADER_SIZE};
@@ -288,8 +319,7 @@ mod tests {
 
     #[test]
     fn empty_allocator() {
-        let mut buffer = Buffer::<32>::new();
-        buffer.ensure_initialization();
+        let buffer = Buffer::<32>::new();
         let expected = Entry::free(32 - 4);
         let actual = unsafe { buffer.at(0).assume_init() };
         assert_eq!(expected, actual);
@@ -321,14 +351,12 @@ mod tests {
 
     #[test]
     fn entry_iter() {
-        let mut buffer = Buffer::<32>::new();
-        buffer.ensure_initialization();
+        let buffer = Buffer::<32>::new();
         let mut iter = buffer.entries();
         assert_eq!(iter.next(), Some(ValidatedOffset(0)));
         assert_eq!(iter.next(), None);
 
         let mut buffer = Buffer::<32>::new();
-        buffer.ensure_initialization();
         buffer.at_mut(0).write(Entry::free(4));
         buffer.at_mut(8).write(Entry::used(4));
         buffer.at_mut(16).write(Entry::free(12));
@@ -342,7 +370,6 @@ mod tests {
     #[test]
     fn indexing() {
         let mut buffer = Buffer::<32>::new();
-        buffer.ensure_initialization();
         buffer.at_mut(8).write(Entry::used(4));
 
         assert_eq!(buffer[ValidatedOffset(8)], Entry::used(4));
@@ -353,8 +380,7 @@ mod tests {
     #[test]
     #[should_panic]
     fn at_out_of_bounds() {
-        let mut buffer = Buffer::<32>::new();
-        buffer.ensure_initialization();
+        let buffer = Buffer::<32>::new();
         buffer.at(64); // panic here
     }
 
@@ -362,15 +388,13 @@ mod tests {
     #[should_panic]
     fn at_mut_out_of_bounds() {
         let mut buffer = Buffer::<32>::new();
-        buffer.ensure_initialization();
         buffer.at_mut(64); // panic here
     }
 
     #[test]
     #[should_panic]
     fn at_unaligned() {
-        let mut buffer = Buffer::<32>::new();
-        buffer.ensure_initialization();
+        let buffer = Buffer::<32>::new();
         buffer.at(2); // panic here
     }
 
@@ -378,14 +402,12 @@ mod tests {
     #[should_panic]
     fn at_mut_unaligned() {
         let mut buffer = Buffer::<32>::new();
-        buffer.ensure_initialization();
         buffer.at_mut(2); // panic here
     }
 
     #[test]
     fn following_free_entry() {
         let mut buffer = Buffer::<24>::new();
-        buffer.ensure_initialization();
         buffer.at_mut(0).write(Entry::used(4));
         buffer.at_mut(8).write(Entry::used(4));
         buffer.at_mut(16).write(Entry::free(4));
@@ -406,7 +428,6 @@ mod tests {
         use core::ptr;
 
         let mut buffer = Buffer::<20>::new();
-        buffer.ensure_initialization();
         buffer.at_mut(0).write(Entry::used(4));
 
         let expected = &buffer.0[4..8];
@@ -417,7 +438,6 @@ mod tests {
     #[test]
     fn mark_used_without_split() {
         let mut buffer = Buffer::<24>::new();
-        buffer.ensure_initialization();
         buffer.at_mut(0).write(Entry::used(4));
         buffer.at_mut(8).write(Entry::free(4));
         buffer.at_mut(16).write(Entry::used(4));
@@ -433,7 +453,6 @@ mod tests {
     #[test]
     fn mark_used_with_split() {
         let mut buffer = Buffer::<32>::new();
-        buffer.ensure_initialization();
         buffer.at_mut(0).write(Entry::used(4));
         buffer.at_mut(8).write(Entry::free(20));
 
